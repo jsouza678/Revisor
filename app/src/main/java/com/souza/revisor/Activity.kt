0cf80b63880d1b77f7774c8097a5f410166dd463
João Souza <jfsouza678@gmail.com>
2020-08-02 14:33:45 -0300

    added CI/CD and ktlint; Koin start function created on Application.

---
+++ app/src/main/java/com/souza/revisor/Activity.kt
@@ -0,0 +1,14 @@
+package com.souza.revisor
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Activity : AppCompatActivity() {
+
+    override fun onCreate(
+        savedInstanceState: Bundle?
+    ) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
